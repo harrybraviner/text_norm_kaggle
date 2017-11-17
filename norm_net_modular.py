8282fa24c21c4363b5f802c189cdb7d1ec35f36f
Harry J. Braviner <harry.braviner@gmail.com>
2017-11-17 22:11:41 +0000

    Beginnings of TranslationNet
    Encoder written. TranslationNet (contains the Encoder and Decoder) is
    partially written.

--- norm_net_modular.py
+++ norm_net_modular.py
@@ -66,9 +66,10 @@ class Encoder(RecurrentNet):
     def __init__(self, params, cell_type = 'LSTM'):
         super(Encoder, self).__init__(params, cell_type)
 
-#    def build(self):
-#        # Call to superclass builds the recurrent part of the net
-#        super(Encoder, self).build_core_rnn()
+    def build(self):
+        # Call to superclass builds the recurrent part of the net
+        super(Encoder, self).build_core_rnn()
+
 #        # FIXME - move the stuff below to the Decoder
 #        #n_in = self._layer_sizes[-1]
 #        #n_out = self._n_out
@@ -106,23 +107,59 @@ class TranslationNet:
     inputs to them via a common embedding matrix.
     
     Args:
-        use_embedding_matrix : bool, whether or not to pass input through an embedding matrix first.
+        embedding_size : int, the size of the embedding vectors to use for the characters. If None, the embedding matrix is the identity matrix (i.e. one-hot encodings are passed straight to the RNN).
 
     """
 
-    def __init__(self, params, cell_type = 'LSTM', use_embedding_matrix = True):
+    def __init__(self, params, cell_type = 'LSTM', embedding_size = 32, mini_dataset = False):
         _assert_like_parameters(params)
         _assert_valid_cell_type(cell_type)
 
+        self._params = params   # Save for passeing to encoder and decoder
         self._max_input_size  = params['max_input_size']
         self._max_output_size = params['max_output_size']
+        self._cell_type = cell_type
+        self._embedding_size = embedding_size
 
         self.built = False
-        self.training_dataset = dataset.TrainingDataset(line_limit = 1000, max_input_size = self._max_input_size,
+
+        # Load the training dataset - will need some properties of this for making the embedding matrix
+        line_limit = 1000 if mini_dataset else None
+        self.training_dataset = dataset.TrainingDataset(line_limit = line_limit, max_input_size = self._max_input_size,
                                                         max_output_size = self._max_output_size)
 
-    #def build(self):
+        # The unnormalized input to the encoder (and the hint input to the decoder)
+        # can be a non-rare character, or the tokens <RARE>, <STOP>, or <START>
+        self._input_rare_ix  = self.training_dataset.num_non_rare_chars
+        self._input_stop_ix  = self.training_dataset.num_non_rare_chars + 1
+        self._input_start_ix = self.training_dataset.num_non_rare_chars + 2
+        self._num_input_tokens = self._input_start_ix + 1
 
+    def build(self):
+        self._unnorm_ix = tf.placeholder(shape = [None, self._num_input_tokens], dtype = tf.int32)
+        if self._embedding_size is not None:
+            self._embedding_matrix = tf.Variable(tf.truncated_normal([self._num_input_tokens, self._embedding_size], mean=0.1, stddev=0.02))
+            self._encoder_input = tf.nn.embedding_lookup(self._embedding_matrix, self._unnorm_ix)
+        else:
+            self._encoder_input = tf.one_hot(indices = self._unnorm_ix, depth = self._num_input_tokens)
+
+        self._unnorm_seq_lengths = tf.placeholder(shape = [None], dtype=tf.int32)
+
+        self._encoder = Encoder(self._params, cell_type = self._cell_type)
+
+        # 'Wire up' the circuit (self._unnorm_ix, self._unnorm_seq_lengths) --> self._encoder_state_out
+        self._encoder_state_out = self._encoder.connect(self._encoder_input, seq_lengths = self._unnorm_seq_lengths)
+
+        # FIXME - need to do the decoder
+
+        self.built = True
+
+
+def _get_shape(t):
+    """Get the shape of a tensor as a list,
+    since that's more convenient for our tests.
+    """
+    return [x.value for x in t.shape]
 
 class LikeParameterTests(unittest.TestCase):
 
@@ -166,17 +203,30 @@ class RecurrentNetTests(unittest.TestCase):
 class EncoderTests(unittest.TestCase):
     
     def test_contruction_and_building(self):
-        params = _example_params
-        net = Encoder(params, cell_type='LSTM')
-        net.build_core_rnn()
-        self.assertTrue(net.built)
-        
-        input_embedded = tf.placeholder(shape=[None, 20, 50], dtype = tf.float32)
-        seq_lengths = tf.placeholder(shape = [None], dtype = tf.int32)
-        final_state = net.connect(inputs = input_embedded, seq_lengths = seq_lengths)
-        # Check that the shape is what we expect
-        self.assertEqual([[[x.value for x in hc.shape] for hc in layer] for layer in final_state],
-                         [[[None, 10], [None, 10]], [[None, 20], [None, 20]]])
+        with tf.variable_scope('EncoderTests'):
+            params = _example_params
+            net = Encoder(params, cell_type='LSTM')
+            net.build_core_rnn()
+            self.assertTrue(net.built)
+            
+            input_embedded = tf.placeholder(shape=[None, 20, 50], dtype = tf.float32)
+            seq_lengths = tf.placeholder(shape = [None], dtype = tf.int32)
+            final_state = net.connect(inputs = input_embedded, seq_lengths = seq_lengths)
+            # Check that the shape is what we expect
+            self.assertEqual([[_get_shape(hc) for hc in layer] for layer in final_state],
+                             [[[None, 10], [None, 10]], [[None, 20], [None, 20]]])
+
+class TranslationNetTests(unittest.TestCase):
+
+    def test_construction_and_geometry(self):
+        with tf.variable_scope('TranslationNetTests'):
+            params = _example_params
+            net = TranslationNet(params, mini_dataset = True)
+            net.build()
+            self.assertTrue(net.built)
+
+            # Should have 3 special tokens: <RARE>, <START>, and <STOP>
+            self.assertEqual(_get_shape(net._unnorm_ix), [None, net.training_dataset.num_non_rare_chars + 3])
 
 # FIXME - add a test to inspect the trainable weights at the end of this!
 # FIXME - add automatic naming of variables?
